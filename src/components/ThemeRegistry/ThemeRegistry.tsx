17803bc821290bb47a1cae7f3c67576177c9e4be
Edu Paz <jooeduardo8@gmail.com>
2023-10-03 20:58:57 -0300

    use dark mode

--- src/components/ThemeRegistry/ThemeRegistry.tsx
+++ src/components/ThemeRegistry/ThemeRegistry.tsx
@@ -1,15 +1,38 @@
 "use client";
-import * as React from "react";
-import { ThemeProvider } from "@mui/material/styles";
+
+import { useMediaQuery } from "@mui/material";
 import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { Roboto } from "next/font/google";
+import { useMemo } from "react";
 import NextAppDirEmotionCacheProvider from "./EmotionCache";
-import theme from "./theme";
+
+const roboto = Roboto({
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export default function ThemeRegistry({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? "dark" : "light",
+        },
+        typography: {
+          fontFamily: roboto.style.fontFamily,
+        },
+      }),
+    [prefersDarkMode]
+  );
+
   return (
     <NextAppDirEmotionCacheProvider options={{ key: "mui" }}>
       <ThemeProvider theme={theme}>
