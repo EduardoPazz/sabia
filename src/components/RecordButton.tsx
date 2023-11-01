299a51bff5ba6426ca8dd9c9ce9a6860cb3bb45d
Edu Paz <jooeduardo8@gmail.com>
2023-10-31 21:09:12 -0300

    feat: add text-to-speech

--- src/components/RecordButton.tsx
+++ src/components/RecordButton.tsx
@@ -1,11 +1,27 @@
 "use client";
 
 import { Button, Stack } from "@mui/material";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+function speech(content: string) {
+  const msg = new SpeechSynthesisUtterance();
+
+  msg.volume = 1; //define o volume do áudio (de 0 a 1)
+  msg.rate = 1; // define a velocidade do áudio (0.1 a 1)
+  msg.pitch = 1; // define o tom em que o áudio é falado ( de 0 a 2)
+  msg.text = content;
+  msg.lang = "pt-BR"; //define o idioma do áudio
+  msg.voice = speechSynthesis.getVoices()[0]; //define a voz a ser usada
+
+  console.log("trying to speak", msg);
+
+  speechSynthesis.speak(msg); //executa a voz
+}
 
 export function RecordButton({ recog }: { recog: SpeechRecognition }) {
   const [isRecording, setIsRecording] = useState(false);
   const [result, setResult] = useState("");
+  const [syllables, setSyllables] = useState("");
 
   recog.onstart = (e) => {
     console.log(e);
@@ -20,9 +36,31 @@ export function RecordButton({ recog }: { recog: SpeechRecognition }) {
   };
 
   recog.onresult = (e) => {
-    setResult(e.results[0][0].transcript);
+    const transcript = e.results[0][0].transcript;
+    setResult(transcript);
     setIsRecording(false);
+    const timeoutId = setTimeout(() => speech(transcript), 1000);
+    return () => clearTimeout(timeoutId);
   };
+
+  useEffect(() => {
+    console.log("starting to process syllables");
+
+    const word = result.split(" ")[0].trim();
+
+    if (word.length == 0) {
+      return;
+    }
+
+    console.log("fetching syllables for", word);
+
+    fetch(`/api/${result}`)
+      .then((res) => res.json())
+      .then((res) => {
+        console.log({ res });
+        setSyllables(res["syllables"]);
+      });
+  }, [result]);
 
   function handleClick() {
     if (isRecording) {
@@ -37,7 +75,7 @@ export function RecordButton({ recog }: { recog: SpeechRecognition }) {
       <Button variant="contained" onClick={handleClick}>
         {isRecording ? "Stop" : "Record"}
       </Button>
-      {result}
+      {syllables}
     </Stack>
   );
 }
